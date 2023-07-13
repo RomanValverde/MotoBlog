2e4a950035d8fe377e9ac12fa158c21bf32a35ac
RomanValverde <romanvalverde@hotmail.com.ar>
2023-07-13 20:57:41 -0300

    create users app

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from Blog.views import *
 
 urlpatterns = [
-    path('', inicio),
+    path('', inicio, name = 'inicio'),    
+    path('Users/', include('Users.urls')),
 
 ]    
--- Blog/views.py
+++ Blog/views.py
@@ -3,8 +3,7 @@ from django.template import loader
 from django.shortcuts import render
 from Blog.models import *
 """ from Blog.forms import * """
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login, logout, authenticate
+
 
 # Create your views here.
 
